28385bbc65fd57a8ae14b94cdd5c912d71c99300
Shpakova Alena <shpakovadev@gmail.com>
2019-10-07 12:04:16 +0300

    axios get page count

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -7,7 +7,7 @@ class Users extends React.Component {
   componentDidMount() {
     console.log('this.props', this.props)
     axios
-      .get('https://social-network.samuraijs.com/api/1.0/users')
+      .get('https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${his.props.pageSize}')
       .then(response => {
         this.props.setUsers(response.data.items)
       })
